a82147a14117b47345fbaf21450d4ed8f23ad05b
Philip Nicoletti <philipnicoletti@Torontos-MacBook-Pro.local>
2019-06-07 11:54:26 -0400

    First Commit

---
+++ src/Components/HeatMap/HeatMap.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import heat from 'heatmapjs';
+
+class HeatMap extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+              this is the heatmap componet
+        </div>
+    );
+  }
+}
+
+export default HeatMap;
---
+++ src/constants.js
@@ -0,0 +1,8 @@
+const imageOne = {
+  topLeft: { lat: 43.691900, long: -79.453116 },
+  bottomLeft: { lat: 43.623000, long: -79.442569 },
+  topRight: { lat: 43.691900, long: -79.329231 },
+  bottomRight: { lat: 43.623000, long: -79.330696 },
+};
+
+export { imageOne };
