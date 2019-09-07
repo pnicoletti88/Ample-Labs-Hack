e3ad3c2f38d0dd8dc78d884769de977ba6ab1a72
Phil <pnicolet@uwo.ca>
2019-09-06 22:44:15 -0400

    Procfile

---
+++ server/build/precache-manifest.6860a73e87b1981e3ba7a9a4eabc298c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5247e3257d8a1fb0684d4a1632fc1eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c63bb27a44c3b94e87a6",
+    "url": "/static/css/2.4118c0c4.chunk.css"
+  },
+  {
+    "revision": "0cbccaeb5ac96623b41b",
+    "url": "/static/css/main.a618cebe.chunk.css"
+  },
+  {
+    "revision": "c63bb27a44c3b94e87a6",
+    "url": "/static/js/2.2155ab5d.chunk.js"
+  },
+  {
+    "revision": "0cbccaeb5ac96623b41b",
+    "url": "/static/js/main.994ff0bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee713939fa5aa8187403d3d3d3899731",
+    "url": "/static/media/clothing.ee713939.svg"
+  },
+  {
+    "revision": "ced1ebf008a53dd6a6303b4550b7db0f",
+    "url": "/static/media/dropIn.ced1ebf0.svg"
+  },
+  {
+    "revision": "c0adc641ec76a221d9458ee76a1207a0",
+    "url": "/static/media/emergency.c0adc641.svg"
+  },
+  {
+    "revision": "9bb213d2089bd83f2fad016f841a5a65",
+    "url": "/static/media/freeMeal.9bb213d2.svg"
+  },
+  {
+    "revision": "af0d3930bf956d7ae5529714989005ab",
+    "url": "/static/media/shelter.af0d3930.svg"
+  }
+]);
--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ require("./mongooseClient");
 const express = require("express");
 const bodyParser = require("body-parser");
 const http = require("http");
+const path = require("path");
 const indexRouter = require("./Routes/index");
 
 const app = express();
@@ -11,6 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api", indexRouter);
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 const server = http.createServer(app);
 server.listen(process.env.PORT, () => {
